18ac6c6c3c5b62bf6847088e6d86b68ff1595176
Eric King <cg100146@comporium.com>
2018-09-26 13:20:54 -0400

    updating blog posts

--- src/components/Bio.js
+++ src/components/Bio.js
@@ -18,7 +18,7 @@ class Bio extends React.Component {
       >
         <img
           src={profilePic}
-          alt={`Kyle Mathews`}
+          alt={`Eric King`}
           style={{
             marginRight: rhythm(1 / 2),
             marginBottom: 0,
@@ -27,9 +27,9 @@ class Bio extends React.Component {
           }}
         />
         <p>
-          Written by <strong>Kyle Mathews</strong> who lives and works in San
-          Francisco building useful things.{' '}
-          <a href="https://twitter.com/kylemathews">
+          Written by <strong>Eric King</strong> who lives in Charlotte, NC and works in Rock
+          Hill building useful things.{' '}
+          <a href="https://twitter.com/thekingdomdev">
             You should follow him on Twitter
           </a>
         </p>
--- src/pages/my-second-post/index.md
+++ src/pages/my-second-post/index.md
@@ -5,9 +5,6 @@ date: "2015-05-06T23:46:37.121Z"
 
 Wow! I love blogging so much already.
 
-Did you know that "despite its name, salted duck eggs can also be made from
-chicken eggs, though the taste and texture will be somewhat different, and the
-egg yolk will be less rich."?
-([Wikipedia Link](http://en.wikipedia.org/wiki/Salted_duck_egg))
+Really craving some scotch right now...
 
-Yeah, I didn't either.
+Sometimes you just need a drink.
