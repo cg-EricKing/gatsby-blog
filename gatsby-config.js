ebe8f0c4c6b31e057e9514cf5a075c3ae5b2821a
Eric King <cg100146@comporium.com>
2018-09-26 14:52:47 -0400

    updated metadata

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,9 +1,8 @@
 module.exports = {
   siteMetadata: {
-    title: 'Gatsby Starter Blog',
-    author: 'Kyle Mathews',
-    description: 'A starter blog demonstrating what Gatsby can do.',
-    siteUrl: 'https://gatsbyjs.github.io/gatsby-starter-blog/',
+    title: 'TheKingdomDev Blog',
+    author: 'Eric King',
+    description: 'A starter blog demonstrating what Gatsby can do.'
   },
   pathPrefix: '/gatsby-starter-blog',
   plugins: [
